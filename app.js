f3cdb9e16a7aafdb853332fd6b7005dabf1deea3
caiirene <aaqqii901@gmail.com>
2023-11-20 17:00:52 -0800

    nodekanbas

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 // const express = require("express");
+import "dotenv/config";
 import express from "express";
 import HelloRoutes from "./hello.js";
-import Lab5 from "./lab5.js";
+import Lab5 from "./Lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+ModuleRoutes(app);
 
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
 HelloRoutes(app);
 
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
