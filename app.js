cd16a0dcf5ceb94285a8b8b91efe4e712a496d02
caiirene <aaqqii901@gmail.com>
2023-12-03 23:00:46 -0800

    a6node

--- app.js
+++ app.js
@@ -5,12 +5,39 @@ import HelloRoutes from "./hello.js";
 import Lab5 from "./Lab55.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import UserRoutes from "./users/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+import session from "express-session";
+import "dotenv/config";
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL
+}));
+
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
+
 app.use(express.json());
+
+
+UserRoutes(app);
 ModuleRoutes(app);
 
 ModuleRoutes(app);
